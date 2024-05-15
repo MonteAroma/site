05f9bf6b83498d30d96d42bf1293ab060844dd84
MonteAroma <montearomasesenciales@gmail.com>
2024-05-14 22:02:44 -0500

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,67 @@
+document.querySelector('#conjunto1').addEventListener('mouseover', function() {
+    document.querySelector('.sectionAromatica').style.display = 'block';
+    document.querySelector('.sectionAromatica').style.background = 'rgba(32, 75, 35, 0.4)';
+});
+
+document.querySelector('#conjunto1').addEventListener('mouseout', function() {
+    document.querySelector('.sectionAromatica').style.display = 'none';
+});
+
+// Section Condimentos 
+
+document.querySelector('#conjunto2').addEventListener('mouseover', function() {
+    document.querySelector('.sectionCondimentos').style.display = 'block';
+    document.querySelector('.sectionCondimentos').style.background = 'rgba(32, 75, 35, 0.4)';
+});
+
+document.querySelector('#conjunto2').addEventListener('mouseout', function() {
+    document.querySelector('.sectionCondimentos').style.display = 'none';
+});
+
+// Section Aceites Esenciales
+
+document.querySelector('#conjunto3').addEventListener('mouseover', function() {
+    document.querySelector('.sectionAceitesE').style.display = 'block';
+    document.querySelector('.sectionAceitesE').style.background = 'rgba(32, 75, 35, 0.4)';
+});
+
+document.querySelector('#conjunto3').addEventListener('mouseout', function() {
+    document.querySelector('.sectionAceitesE').style.display = 'none';
+});
+
+// Section Aceites Macerados
+
+document.querySelector('#conjunto4').addEventListener('mouseover', function() {
+    document.querySelector('.sectionAceitesM').style.display = 'block';
+    document.querySelector('.sectionAceitesM').style.background = 'rgba(32, 75, 35, 0.4)';
+});
+
+document.querySelector('#conjunto4').addEventListener('mouseout', function() {
+    document.querySelector('.sectionAceitesM').style.display = 'none';
+});
+
+
+// Modal Aromaticas
+
+var abrir = document.querySelector('#BAromaticas'); 
+var modalAromaticas = document.querySelector('#modal-Aromaticas');
+
+abrir.addEventListener('click',()=> {
+
+    modalAromaticas.style.display = 'block';
+
+});
+
+
+// Modal Condimentos
+
+var abrirC = document.querySelector('#BCondimentos'); 
+var modalCondimentos = document.querySelector('#modal-Condimentos');
+
+abrirC.addEventListener('click',()=> {
+
+    modalCondimentos.style.display = 'block';
+
+});
+
+
